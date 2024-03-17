0a798504e81d9b9ab16882685e8a2b85f2018ea4
ljabakji <ljabakji@uncc.edu>
2024-03-17 18:21:18 -0400

    modified webserver.go in Lab4

--- labs/Lab4/webserver.go
+++ labs/Lab4/webserver.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/list", db.list)
-	mux.HandleFunc("/price", db.price)
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandleFunc(db.price))
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
@@ -28,10 +28,45 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if price, ok := db[item]; ok {
-		fmt.Fprintf(w, "%s\n", price)
-	} else {
+	price, ok := db[item]
+	if !ok{
+
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+
+
 	}
+		fmt.Fprintf(w, "%s\n", price)
+	
+		
+	}
+
+
+
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound) // 404
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+	default:
+		w.WriteHeader(http.StatusNotFound)//404
+		fmt.Fprintf(w, "no such page: %s\n",req.URL)
+
+	}
+}
+
+type HandlerFunc func(w ResponseWriter, r *Request)
+func (f HandlerFunc) ServerHTTP(w ResponseWriter, r *Request {
+	f(w, r)
 }
