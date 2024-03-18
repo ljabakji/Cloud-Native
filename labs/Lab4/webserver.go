ae37badf5638cf3a56bc5a6bba9c4ffb0a73c0a5
ljabakji <ljabakji@uncc.edu>
2024-03-18 00:42:34 -0400

    finished lab7

--- labs/Lab4/webserver.go
+++ labs/Lab4/webserver.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,11 +11,13 @@ func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/list", http.HandlerFunc(db.list))
-	mux.HandleFunc("/create", db.create)
-	mux.HandleFunc("/update", db.update)
-	mus.HandleFunc("/delete", db.delete)
+	mux.HandleFunc("/list2",http.HandlerFunc(db.list))
+	// mux.HandleFunc("/create", db.create)
+	// mux.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe(":8000", mux))
+	
+	
 
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
 type dollars float32
@@ -25,6 +27,11 @@ func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 type database map[string]dollars
 
 func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+func (db database) list2(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
@@ -44,58 +51,63 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func (db database) create(w http.ResponseWriter, req *http.Request) {
-	var newItem struct {
-		Item  string  `json:"item"`
-		Price float32 `json:"price"`
-	}
-	err := json.NewDecoder(req.Body).Decode(&newItem)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+// func (db database) create(w http.ResponseWriter, req *http.Request) {
+// 	var newItem struct {
+// 		Item  string  `json:"item"`
+// 		Price float32 `json:"price"`
+// 	}
+// 	err := json.NewDecoder(req.Body).Decode(&newItem)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
 
-	if _, ok := db[newItem.Item]; ok {
-		http.Error(w, "Item already exists", http.StatusBadRequest)
-		return
-	}
+// 	if _, ok := db[newItem.Item]; ok {
+// 		http.Error(w, "Item already exists", http.StatusBadRequest)
+// 		return
+// 	}
 
-	db[newItem.Item] = dollars(newItem.Price)
-	w.WriteHeader(http.StatusCreated)
-}
+// 	db[newItem.Item] = dollars(newItem.Price)
+// 	w.WriteHeader(http.StatusCreated)
+// }
 
-func (db database) update(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	if _, ok := db[item]; !ok {
-		http.Error(w, "Item not found", http.StatusNotFound)
-		return
-	}
+// func (db database) update(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
+// 	if _, ok := db[item]; !ok {
+// 		http.Error(w, "Item not found", http.StatusNotFound)
+// 		return
+// 	}
 
-	var updatedPrice float32
-	err := json.NewDecoder(req.Body).Decode(&updatedPrice)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+// 	var updatedPrice float32
+// 	err := json.NewDecoder(req.Body).Decode(&updatedPrice)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
 
-	db[item] = dollars(updatedPrice)
-	w.WriteHeader(http.StatusOk)
-}
+// 	db[item] = dollars(updatedPrice)
+// }
 
-func (db database) delete(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	if _, ok := db[item]; !ok {
+// func (db database) delete(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
+// 	if _, ok := db[item]; !ok {
 
-		http.Error(W, "Item not found", http.StatusNotFound)
-		return
-	}
-	delete(db, item)
-	w.WriteHeader(http.StatusOk)
-}
+// 		http.Error(w, "Item not found", http.StatusNotFound)
+// 		return
+// 	}
+// 	delete(db, item)
+// 	fmt.Fprintf(w, "Deleted item :%s\n", item)
+
+// }
+
 
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
 	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/list2":
 		for item, price := range db {
 			fmt.Fprintf(w, "%s: %s\n", item, price)
 		}
@@ -108,6 +120,8 @@ func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		fmt.Fprintf(w, "%s\n", price)
+
+	
 	default:
 		w.WriteHeader(http.StatusNotFound) //404
 		fmt.Fprintf(w, "no such page: %s\n", req.URL)
--- labs/Lab7/webserver.go
+++ labs/Lab7/webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	// "encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,9 +10,14 @@ import (
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/list", db.list)
-	mux.HandleFunc("/price", db.price)
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	mux.HandleFunc("/list", http.HandlerFunc(db.list))
+	
+	// mux.HandleFunc("/create", db.create)
+	// mux.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe(":8000", mux))
+	
+	
+
 }
 
 type dollars float32
@@ -28,10 +34,89 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if price, ok := db[item]; ok {
-		fmt.Fprintf(w, "%s\n", price)
-	} else {
+	price, ok := db[item]
+	if !ok {
+
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+
+	}
+	fmt.Fprintf(w, "%s\n", price)
+
+}
+
+// func (db database) create(w http.ResponseWriter, req *http.Request) {
+// 	var newItem struct {
+// 		Item  string  `json:"item"`
+// 		Price float32 `json:"price"`
+// 	}
+// 	err := json.NewDecoder(req.Body).Decode(&newItem)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	if _, ok := db[newItem.Item]; ok {
+// 		http.Error(w, "Item already exists", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	db[newItem.Item] = dollars(newItem.Price)
+// 	w.WriteHeader(http.StatusCreated)
+// }
+
+// func (db database) update(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
+// 	if _, ok := db[item]; !ok {
+// 		http.Error(w, "Item not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	var updatedPrice float32
+// 	err := json.NewDecoder(req.Body).Decode(&updatedPrice)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	db[item] = dollars(updatedPrice)
+// }
+
+// func (db database) delete(w http.ResponseWriter, req *http.Request) {
+// 	item := req.URL.Query().Get("item")
+// 	if _, ok := db[item]; !ok {
+
+// 		http.Error(w, "Item not found", http.StatusNotFound)
+// 		return
+// 	}
+// 	delete(db, item)
+// 	fmt.Fprintf(w, "Deleted item :%s\n", item)
+
+// }
+
+
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound) // 404
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+
+	
+	default:
+		w.WriteHeader(http.StatusNotFound) //404
+		fmt.Fprintf(w, "no such page: %s\n", req.URL)
+
 	}
 }
